d37a612913760df02e92e7db803163afeeae56f5
LoKi <sand5088@gmail.com>
2022-02-24 17:21:13 +0700

    init models

---
+++ db/schema.rb
@@ -0,0 +1,25 @@
+ActiveRecord::Schema.define(version: 2022_02_24_101940) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "product_category"
+    t.integer "stock"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "categoty_id"
+    t.index ["categoty_id"], name: "index_products_on_categoty_id"
+  end
+
+end
