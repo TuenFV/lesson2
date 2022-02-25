4b66c48995f60b3e7c9811b6ae0cac72b9e6ef74
LoKi <sand5088@gmail.com>
2022-02-25 17:10:22 +0700

    Add controller-product and update UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_101940) do
+ActiveRecord::Schema.define(version: 2022_02_25_080015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2022_02_24_101940) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "categoty_id"
-    t.index ["categoty_id"], name: "index_products_on_categoty_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
