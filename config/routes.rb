03cd8785c2fd8a4e84088fa52fe70bc3c3bfa5d7
LoKi <sand5088@gmail.com>
2022-02-25 17:54:43 +0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :categories
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'admin/products#index'
 end
